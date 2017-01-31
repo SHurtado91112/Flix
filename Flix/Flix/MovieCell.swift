27d9451521e8d4eaf206deb7993d2f3e59239823
SH-Arrow <shurt91112@gmail.com>
2017-01-31 15:21:48 -0500

    general layout and functionality, extended through collection view

---
+++ Flix/Flix/MovieCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieCell.swift
+//  Flix
+//
+//  Created by Steven Hurtado on 1/30/17.
+//  Copyright © 2017 Steven Hurtado. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UICollectionViewCell {
+    
+}
---
+++ Flix/Flix/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Steven Hurtado on 1/30/17.
+//  Copyright © 2017 Steven Hurtado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
